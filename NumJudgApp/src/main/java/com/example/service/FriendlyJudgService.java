6cd98dabb3314a10dd41371fc5ebb53cf2b68ab9
koto <koto@localhost>
2026-02-02 14:39:25 +0900

    Revert "Revert "完全数判定の実装""
    This reverts commit 08f481d1dbd6c7583d7f0018c04f80df5bdfa89f.

---
+++ NumJudgApp/src/main/java/com/example/service/FriendlyJudgService.java
@@ -0,0 +1,7 @@
+package com.example.service;
+
+public interface FriendlyJudgService {
+
+	/** 友愛数判定 */
+	public boolean friendlyJudg(int inputNum);
+}
--- NumJudgApp/src/main/java/com/example/service/impl/NumJudgServiceImpl.java
+++ NumJudgApp/src/main/java/com/example/service/impl/NumJudgServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.model.MNum;
 import com.example.repository.NumMapper;
 import com.example.service.EvenJudgService;
 import com.example.service.NumJudgService;
+import com.example.service.PerfectJudgService;
 import com.example.service.PrimeJudgService;
 
 @Service
@@ -18,6 +19,8 @@ public class NumJudgServiceImpl implements NumJudgService {
 	private EvenJudgService evenJudgService;
 	@Autowired
 	private PrimeJudgService primeJudgService;
+	@Autowired
+	private PerfectJudgService perfectJudgService;
 	
 	/** 入力番号登録 */
 	@Override
@@ -29,7 +32,9 @@ public class NumJudgServiceImpl implements NumJudgService {
 	public String numJudg(int inputNum) {
 		String result = "未判定";
 		
-		if(primeJudgService.primeJudg(inputNum)) {
+		if(perfectJudgService.perfectJudg(inputNum)) {
+			result = "完全数";
+		} else if(primeJudgService.primeJudg(inputNum)) {
 			result = "素数";
 		} else if(evenJudgService.evenJudg(inputNum)) {
 			result = "偶数";
